ecc8784fa04f1a185374617b50e401e8560cb0b9
Axel Torres <axlalet@icloud.com>
2024-06-24 14:24:22 -0600

    test logout

--- tutorias/admin/js/controladorDinamicoAdmin.js
+++ tutorias/admin/js/controladorDinamicoAdmin.js
@@ -1,7 +1,20 @@
-$(document).ready(controlador);
+$(document).ready(function() {
 
-function controlador() {
-    
-    
-    
-}
+    $('#cerrar_sesion').click(function(e) {
+        e.preventDefault(); // Previene la acción por defecto del botón
+
+        // Envía una solicitud AJAX a logout.php
+        $.ajax({
+            url: './php/logout.php',
+            type: 'POST',
+            success: function(response) {
+                // Redirige a index.html después de cerrar sesión
+                window.location.href = '../index.html';
+            },
+            error: function() {
+                // Maneja errores aquí, si los hay
+                alert('Error al cerrar sesión');
+            }
+        });
+    });
+});
